d7ec9caf7615fc72c6d25b10f212e9440d1bb7ad
sabatms <saba.tms15@gmail.com>
2023-09-18 00:50:05 -0700

    add index.html file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const numbers = document.querySelectorAll('.numbers');
 const result = document.querySelector('.result span');
 const signs = document.querySelectorAll('.sign');
-const equals = document.querySelector('equals');
+const equals = document.querySelector('.equals');
 const clear = document.querySelector('.clear');
 const negative =document.querySelector('.negative');
 const percent =document.querySelector('.percent');
@@ -34,7 +34,7 @@ function getFirstValue(el) {
 }
 
 function getSecondValue(el) {
-    if(firstValue != "" && sign != ""){
+    if(firstValue != "" && sign != "") {
           secondValue += el;
           result.innerHTML = secondValue;
           secondValue = +secondValue;
@@ -51,46 +51,49 @@ function getSign() {
 }
 getSign();
 
-equals.addEventListener ('click', () => {
-          result.innerHTML ="";
-          if(sign === "+"){
-                    resultValue = firstValue + secondValue;
+equals.addEventListener('click', () =>{
+    result.innerHTML = "";
+    if(sign ==="+") {
+        resultValue = firstValue + secondValue;
+    } else if(sign ==="-") {
+        resultValue = firstValue - secondValue;
+    } else if(sign ==="x") {
+        resultValue = firstValue * secondValue;
+    } else if(sign ==="/") {
+        resultValue = firstValue / secondValue;
+    }
+    result.innerHTML = resultValue;
+    firstValue = resultValue;
+    secondValue = "";
 
-          } else if (sign === "-"){
-                    resultValue = firstValue - secondValue;
-                    
-          } else if (sign === "x"){
-                    resultValue = firstValue * secondValue;
-                    
-          } else if (sign === "/"){
-                    resultValue = firstValue / secondValue;
-                    
-          }
-          result.innerHTML = resultValue;
-});
+    checkResultLength()
+
+})
+
 function checkResultLength(){
           resultValue = JSON.strgnify(resultValue);
 
           if(resultValue.length >=8){
                     resultValue = JSON.parse(resultValue);
-                    result.innerHTML = resultValue.toFixed()
+                    result.innerHTML = resultValue.toFixed(5);
           }
 }
 
 negative.addEventListener('click',() =>{
           result.innerHTML ="";
-          if(firstValue != ""){
+          if(firstValue != "") {
                     resultValue = -firstValue;
                     firstValue = resultValue
           }
-          if(firstValue != ""&& secondValue != "" && sign != ""){
+          if(firstValue != "" && secondValue != "" && sign != ""){
                     resultValue = -resultValue;
           }
+
           result.innerHTML = resultValue;
 })
 
-percent.addEventListener('click',() =>{
-          result.innerHTML ="";
+percent.addEventListener('click',() => {
+          result.innerHTML = "";
           if(firstValue != ""){
                     resultValue = firstValue / 100;
                     firstValue = resultValue
@@ -98,5 +101,17 @@ percent.addEventListener('click',() =>{
           if(firstValue != ""&& secondValue != "" && sign != ""){
                     resultValue = resultValue / 100;
           }
+
           result.innerHTML = resultValue;
+})
+
+clear.addEventListener('click', () =>{
+    result.innerHTML = 0;
+
+    firstValue = "";
+    isFirstValue = false;
+    secondValue= "";
+    isSecondValue = false;
+    sign = "";
+    resultValue = 0;
 })
